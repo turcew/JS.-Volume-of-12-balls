6ebc78826a45c9cd9559c327f527feda091e511e
turcew <120652745+turcew@users.noreply.github.com>
2026-04-11 11:17:12 +0200

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,8 @@
+let sumVolume = 0;
+
+for (let i = 1; i <= 12; i++) {
+  const radius = 0.24 + (i - 1) * 0.01;
+  sumVolume += (4 / 3) * Math.PI * radius ** 3;
+}
+
+console.log(`Сумма об'єму куль: ${sumVolume} м³`);
