e7d6f6d3d69ac1c629d0162bc476bad392834f48
turcew <120652745+turcew@users.noreply.github.com>
2026-04-11 13:19:57 +0300

    Refactor volume calculation with constants for radius

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 let sumVolume = 0;
+const step = 0.01;
+const minRadius = 0.24;
 
 for (let i = 1; i <= 12; i++) {
-  const radius = 0.24 + (i - 1) * 0.01;
+  const radius = minRadius + (i - 1) * step;
   sumVolume += (4 / 3) * Math.PI * radius ** 3;
 }
 
